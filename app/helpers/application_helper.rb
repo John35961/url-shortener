e612986fa6dc173c62947a909977503a04d67fac
Jean <contact@jeanguillet.com>
2024-09-30 19:02:34 +0200

    added flash alert color style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(key)
+    case key
+    when 'notice'
+      'bg-green-100 text-green-900'
+    when 'alert'
+      'bg-red-100 text-red-900'
+    else
+      'bg-gray-100 text-gray-900'
+    end
+  end
 end
