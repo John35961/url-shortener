7e7a4158317ef384b0a680c0fd013900cea65acf
Jean <contact@jeanguillet.com>
2024-09-30 21:32:15 +0200

    added Link#expired?

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,11 +2,13 @@ class Link < ApplicationRecord
   validates :original_url, presence: true, format: URI::regexp(%w[http https])
   validates :slug, uniqueness: true
 
-  default_scope { active }
-
   scope :active, -> { where('expires_at IS NULL OR expires_at > ?', Time.current) }
 
   def active?
     expires_at.nil? || expires_at > Time.current
+  end
+
+  def expired?
+    expires_at.present? && expires_at < Time.current
   end
 end
