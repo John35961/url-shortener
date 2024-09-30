2d00487bea0f33a266cadca2bc9a7cbce21d8d25
Jean <contact@jeanguillet.com>
2024-09-30 22:08:23 +0200

    refactored destroy and detroy_all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'links#index'
 
-  resources :links, param: :slug, only: [:new, :create, :index, :show, :destroy] do
+  resources :links, param: :slug, only: [:new, :create, :index, :show] do
+    member do
+      delete :destroy
+    end
+
     collection do
-      delete :destroy_all
+      delete :destroy, action: :destroy_all
     end
   end
 
